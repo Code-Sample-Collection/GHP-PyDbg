015b1eacd0d02db1e71cd20d5be88a76dee1af05
0u0 <wo@wo-class.cn>
2017-01-19 17:40:16 +0800

    v0.5
    基本实现设置int3断点，兵检测断点中断情况，输出中译版一致。
    Bug:有时会出现0xC0000374——堆损坏错误
    单步调试未发现问题，有待进一步测试

--- ghp_pydbg.py
+++ ghp_pydbg.py
@@ -10,6 +10,9 @@ class debugger():
         self.debugger_active    = False
         self.h_thread           = None
         self.context            = None
+        self.breakpoints        = {}
+        self.first_breakpoint   = True
+
         self.exception          = None
         self.exception_address  = None
 
@@ -54,7 +57,7 @@ class debugger():
             print "[*] Error: 0x%08x." % kernel32.GetLastError()
 
     def open_process(self, pid):
-        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, pid, False)
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
         return h_process
 
     def attach(self, pid):
@@ -80,10 +83,11 @@ class debugger():
         debug_event     = DEBUG_EVENT()
         continue_status = DBG_CONTINUE
 
-        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+        if kernel32.WaitForDebugEvent(byref(debug_event), 100):
             # Let's obtain the thread and context information
-            self.h_thread   = self.open_thread(debug_event.dwThreadId)
-            self.context    = self.get_thread_context(self.h_thread)
+            self.h_thread       = self.open_thread(debug_event.dwThreadId)
+            self.context        = self.get_thread_context(self.h_thread)
+            self.debug_event    = debug_event
 
             print "Event Code: %d Thread ID: %d" % (debug_event.dwDebugEventCode,
                                                     debug_event.dwThreadId)
@@ -111,9 +115,33 @@ class debugger():
             kernel32.ContinueDebugEvent(debug_event.dwProcessId,
                                         debug_event.dwThreadId,
                                         continue_status)
+
     def exception_handler_breakpoint(self):
-        print "[*] Inside the breakpoint handler."
         print "[*] Exception address: 0x%08x" % self.exception_address
+        # check if the breakpoint is one that we set
+        if not self.breakpoints.has_key(self.exception_address):
+
+            # if it is the first Windows driven breakpoint
+            # then let's just continue on
+            if self.first_breakpoint:
+                self.first_breakpoint = False
+                print "[*] Hit the first breakpoint."
+                return DBG_CONTINUE
+
+        else:
+            print "[*] Hit user defined breakpoint."
+            # this is where we handle the breakpoints we set
+            # first put the original byte back
+            self.write_process_memory(self.exception_address, self.breakpoints[self.exception_address])
+
+            # obtain a fresh context record, reset EIP back to the
+            # original byte and then set the thread's context record
+            # with the new EIP value
+            self.context = self.get_thread_context(h_thread=self.h_thread)
+            self.context.Rip -= 1
+
+            kernel32.SetThreadContext(self.h_thread, byref(self.context))
+
         return DBG_CONTINUE
 
     def detach(self):
@@ -153,16 +181,71 @@ class debugger():
         else:
             return False
 
-
-    def get_thread_context(self, thread_id):
-
+    def get_thread_context(self, thread_id=None, h_thread=None):
         context64 = WOW64_CONTEXT()
         context64.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
 
         # Obtain a handle to the thread
-        h_thread = self.open_thread(thread_id)
+        if h_thread is None:
+            h_thread = self.open_thread(thread_id)
         if kernel32.GetThreadContext(h_thread, byref(context64)):
             kernel32.CloseHandle(h_thread)
             return context64
         else:
             return False
+
+    def read_process_memory(self,address,length):
+        data        = ""
+        read_buf    = create_string_buffer(length)
+        count       = c_ulong(0)
+
+        if not kernel32.ReadProcessMemory(self.h_process,
+                                          c_longlong(address),
+                                          read_buf,
+                                          length,
+                                          byref(count)):
+            return False
+        else:
+            data += read_buf.raw
+            return data
+
+    def write_process_memory(self,address,data):
+        count   = c_ulong(0)
+        length  = len(data)
+        c_data  = c_char_p(data[count.value:])
+
+        if not kernel32.WriteProcessMemory(self.h_process,
+                                           c_longlong(address),
+                                           c_data,
+                                           length,
+                                           byref(count)):
+            return False
+        else:
+            return True
+
+    def bp_set(self,address):
+        if not self.breakpoints.has_key(address):
+            try:
+                original_byte = self.read_process_memory(address, 1)
+
+                self.write_process_memory(address, "\xCC")
+
+                self.breakpoints[address] = original_byte
+            except:
+                return False
+            return True
+
+    def func_resolve(self,dll,function):
+        # 不指定返回值，python会将返回值默认当做int处理，导致溢出
+        kernel32.GetModuleHandleA.restype = c_ulonglong
+        kernel32.GetProcAddress.restype = c_longlong
+
+        handle  = c_longlong(kernel32.GetModuleHandleA(dll))
+        # 此时handle的实际类型为long，需要再次转换成 longlong，否则会报错 long int too long to convert
+        address = kernel32.GetProcAddress(handle, function)
+
+        kernel32.CloseHandle(handle)
+        return address
+
+
+
--- ghp_test.py
+++ ghp_test.py
@@ -5,5 +5,9 @@ debugger = ghp_pydbg.debugger()
 pid = raw_input("Enter the PID of the process to attach to: ")
 
 debugger.attach(int(pid))
+
+print_address = debugger.func_resolve("msvcrt.dll","printf")
+print "[*] Address of printf: 0x%08x" % print_address
+debugger.bp_set(print_address)
+
 debugger.run()
-debugger.detach()
