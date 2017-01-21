4c994a1f43479254648f18887e903a668a58e97f
0u0 <wo@wo-class.cn>
2017-01-21 10:42:21 +0800

    v0.6
    实现硬件断点

--- ghp_test.py
+++ ghp_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import ghp_pydbg
+from gph_pydbg_defines import HW_EXECUTE
 
 debugger = ghp_pydbg.debugger()
 pid = raw_input("Enter the PID of the process to attach to: ")
@@ -8,6 +9,6 @@ debugger.attach(int(pid))
 
 print_address = debugger.func_resolve("msvcrt.dll","printf")
 print "[*] Address of printf: 0x%08x" % print_address
-debugger.bp_set(print_address)
+debugger.bp_set_hw(print_address,1,HW_EXECUTE)
 
 debugger.run()
